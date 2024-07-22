451df6ed9a87f5168dbeca7ad42622e36738f237
Ivan-ISS <vanya5@inbox.ru>
2024-07-23 01:39:36 +0500

    project_dep: suspense remove

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import { Suspense } from 'react';
-import {LazyShop} from '@/Components/Pages/Shop/Shop.lazy';
-import {LazyAbout} from '@/Components/Pages/About/About.lazy';
+import Shop from '@/Components/Pages/Shop/Shop';
+import About from '@/Components/Pages/About/About';
 
 export default function Content() {
 
     return (
         <Routes>
-            <Route path={ '/about' } element={<Suspense fallback={'loading...'}><LazyAbout /></Suspense>}></Route>
-            <Route path={ '/shop' } element={<Suspense fallback={'loading...'}><LazyShop /></Suspense>}></Route>
+            <Route path={ '/about' } element={<About />}></Route>
+            <Route path={ '/shop' } element={<Shop />}></Route>
         </Routes>
     );
 }
