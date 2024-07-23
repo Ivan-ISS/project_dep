e1e10cdb094c21ccefe21e3eaace7797183f5f61
Ivan-ISS <vanya5@inbox.ru>
2024-07-23 14:33:21 +0500

    project_dep: build folder remove

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import Shop from '@/Components/Pages/Shop/Shop';
-import About from '@/Components/Pages/About/About';
+import { Suspense } from 'react';
+import {LazyShop} from '@/Components/Pages/Shop/Shop.lazy';
+import {LazyAbout} from '@/Components/Pages/About/About.lazy';
 
 export default function Content() {
 
     return (
         <Routes>
-            <Route path={ '/about' } element={<About />}></Route>
-            <Route path={ '/shop' } element={<Shop />}></Route>
+            <Route path={ '/about' } element={<Suspense fallback={'loading...'}><LazyAbout /></Suspense>}></Route>
+            <Route path={ '/shop' } element={<Suspense fallback={'loading...'}><LazyShop /></Suspense>}></Route>
         </Routes>
     );
 }
